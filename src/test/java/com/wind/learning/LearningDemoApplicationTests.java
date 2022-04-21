dd9784bf194d37283d71ff3ce8a50f06f2a8b489
lxz <liuxianzhi>
2022-04-21 09:40:19 +0800

    学习demo项目初始化

---
+++ src/test/java/com/wind/learning/LearningDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wind.learning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
